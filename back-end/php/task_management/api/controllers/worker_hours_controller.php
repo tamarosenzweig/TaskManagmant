de4a148500d491740f2577b6267bcad00355a0fc
tamar.rosenzweig@seldatinc.com <tamar.rosenzweig@seldatinc.com>
2018-12-11 10:24:06 +0200

    11/12/18

--- back-end/php/task_management/api/controllers/worker_hours_controller.php
+++ back-end/php/task_management/api/controllers/worker_hours_controller.php
@@ -2,14 +2,20 @@
 
 class worker_hours_controller extends controller_singletone {
 
+    //get
+    //route=getAllWorkerHours
     public function get_all_worker_hours($params) {
         return $this->service->get_all_worker_hours($params['workerId']);
     }
 
+    //put
+    //route=editWorkerHours
     public function edit_worker_hours($params) {
         return $this->service->edit_worker_hours($params);
     }
 
+    //get
+    //route=hasUncompletedHours
     public function has_uncomleted_hours($params) {
         return $this->service->has_uncomleted_hours($params['workerId'], json_decode($params['projectIdList']));
     }
